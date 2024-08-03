b9dea5f4132ac9e13448f46ae076b320a9e8146e
Ravindu Deshan <Droidcasts@protonmail.com>
2024-08-03 09:07:35 +0000

    Attempt to fix KVM

--- arch/arm64/include/asm/virt.h
+++ arch/arm64/include/asm/virt.h
@@ -77,10 +77,21 @@ void __hyp_set_vectors(phys_addr_t phys_vector_base);
 void __hyp_reset_vectors(void);
 
 /* Reports the availability of HYP mode */
+
 static inline bool is_hyp_mode_available(void)
 {
-	return (__boot_cpu_mode[0] == BOOT_CPU_MODE_EL2 &&
-		__boot_cpu_mode[1] == BOOT_CPU_MODE_EL2);
+    /*
+     * If KVM protected mode is initialized, all CPUs must have been booted
+     * in EL2. Avoid checking __boot_cpu_mode as CPUs now come up in EL1.
+     */
+    if (IS_ENABLED(CONFIG_KVM) &&
+        static_branch_likely(&kvm_protected_mode_initialized))
+        return true;
+
+    /*
+    return (__boot_cpu_mode[0] == BOOT_CPU_MODE_EL2 &&
+        __boot_cpu_mode[1] == BOOT_CPU_MODE_EL2);*/
+    return 1;
 }
 
 /* Check if the bootloader has booted CPUs in different modes */
