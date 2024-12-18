dfb20dfb0200a059e015571272fef8265b437484
Shafeeq Ahmad <124941348+Ahashaf@users.noreply.github.com>
2024-12-18 23:29:22 +0000

    Possible Build error fix

--- drivers/kernelsu/ksu.c
+++ drivers/kernelsu/ksu.c
@@ -38,6 +38,10 @@ unsigned int get_ksu_state(void)
 #include <linux/susfs.h>
 #endif
 
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs.h>
+#endif
+
 static struct workqueue_struct *ksu_workqueue;
 
 bool ksu_queue_work(struct work_struct *work)
@@ -81,6 +85,10 @@ int __init ksu_kernelsu_init(void)
 	pr_alert("**                                                         **");
 	pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
 	pr_alert("*************************************************************");
+#endif
+
+#ifdef CONFIG_KSU_SUSFS
+	susfs_init();
 #endif
 
 #ifdef CONFIG_KSU_SUSFS
